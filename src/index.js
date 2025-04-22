46994bc0c5910bff09282df1e3e1fb915cb94a91
karoldmejia <karoldmejiaoz@gmail.com>
2025-04-22 08:38:55 -0500

    Añadir página notfound.html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const express = require("express");
-const user = require("./control/usercontroller");
+const express = require("express")
+const path = require('path');
+const user = require("./control/usercontroller")
 
-const app = express();
-const port = 5000;
+const app = express()
+app.use(express.json())
 
-app.use(express.json());
+const port = 5000
 
-app.get("/users", user.list);
-app.get("/users/:id", user.get);
-app.post("/users", user.create);
-app.put("/users/:id", user.update);
-app.delete("/users/:id", user.delete);
+// Creando los endpoints para la gestion de usuarios:
+app.get("/users", user.list)
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+app.get("/users/:id", user.get)
+
+app.post("/users/", user.create)
+
+app.put("/users/:id", user.update)
+
+app.delete("/users/:id", user.delete)
+
+app.get('/{*path}', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '..', 'public' , 'notfound.html'))
+})
+
+app.listen(port)
