b0b7fbb9404d2a8dd77a47ec0bbe30320f7652c2
karoldmejia <karoldmejiaoz@gmail.com>
2025-04-22 07:59:05 -0500

    Corrección de manejo de excepciones

--- src/control/usercontroller.js
+++ src/control/usercontroller.js
@@ -1,71 +1,87 @@
-const dbConnection = require("../connection/database");
-const User = require("../model/user");
+const dbConnection = require("../connection/database")
+const user = require("../model/user")
 
 const UserController = {
-  list: (req, res) => {
-    const db = dbConnection.readDB();
-    res.status(200).json(db.users);
-  },
+    // GET -> Listar
+    list: (req, res) => {
+        const db = dbConnection.readDB()
+        res.status(200).json(db.users)
+    }, 
+    get: (req, res) => {
+        try{
+            const db = dbConnection.readDB(); 
+            const id = req.params.id;
+            const userGet = db.users.find(user => user.id == id)
+            if(!userGet){
+                res.status(404).json({error: "user not found"})
+            }
+            else{
+                res.status(200).send( userGet )
+            }
+        }
+        catch(error){
+            res.status(500).json({error: error.message})
+        }
+    },
+    // POST 
+    create: (req, res) => {
+        try {
+            const db = dbConnection.readDB()
+            const userData = req.body
+            const newUser = new user(userData.name, userData.email)
 
-  get: (req, res) => {
-    const db = dbConnection.readDB();
-    const id = parseInt(req.params.id);
-    const userGet = db.users.find(user => user.id === id);
+            newUser.id = db.users.length ? db.users[db.users.length - 1].id + 1 : 1;
 
-    if (!userGet) {
-      return res.status(404).json({ error: "User not found" });
+            db.users.push(newUser)
+            dbConnection.writDB(db)
+
+            res.status(201).send("create user user")
+        }
+        catch(error){
+            res.status(500).json({error: error.message })
+        }
+    },
+    // PUT 
+    update: (req, res) => {
+        try{
+            const db = dbConnection.readDB();
+            const userData = req.body
+            const id = parseInt(req.params.id);
+            const userIndex = db.users.findIndex(user => user.id === id)
+
+            if(userIndex === -1){
+                res.status(404).json({error: "User Not Found"})
+            }
+            else{
+                db.users[userIndex] = { id, ...userData };
+                dbConnection.writDB(db)
+                res.status(204).send()
+            }
+        }
+        catch(error){
+            res.status(500).json({error: error.message})
+        }
+    }, 
+    delete: (req, res) => {
+
+        try{
+            const db = dbConnection.readDB(); 
+            const id = req.params.id;
+            const userIndex = db.users.findIndex(user => user.id == id)
+
+            if(userIndex === -1){
+                res.status(404).json({error: "User Not Found"})
+            }
+            else{
+                db.users.splice(userIndex, 1); 
+                dbConnection.writDB(db)
+                res.status(204).send()
+            }
+        }
+        catch(error){
+            res.status(500).json({error: error.message})
+        }
     }
-
-    res.status(200).json(userGet);
-  },
-
-  create: (req, res) => {
-    try {
-      const db = dbConnection.readDB();
-      const userData = req.body;
-
-      const newUser = new User(userData.name, userData.email);
-
-      newUser.id = db.users.length ? db.users[db.users.length - 1].id + 1 : 1;
-
-      db.users.push(newUser);
-      dbConnection.writeDB(db);
-
-      res.status(201).send("User created");
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
-
-  update: (req, res) => {
-    const db = dbConnection.readDB();
-    const id = parseInt(req.params.id);
-    const userData = req.body;
-
-    const userIndex = db.users.findIndex(user => user.id === id);
-
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    db.users[userIndex] = { id, ...userData };
-    dbConnection.writeDB(db);
-    res.status(200).send("User updated");
-  },
-
-  delete: (req, res) => {
-    const db = dbConnection.readDB();
-    const id = parseInt(req.params.id);
-    const userIndex = db.users.findIndex(user => user.id === id);
-
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    db.users.splice(userIndex, 1);
-    dbConnection.writeDB(db);
-    res.status(200).send("User deleted");
-  }
-};
+}
 
 module.exports = UserController;
